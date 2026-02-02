a9f2d5780e631bd7c3771f34d9fac11a0e1f315c
deefyouknow <deefyouknow@gmail.com>
2026-02-02 21:51:30 +0700

    setup page not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,11 +2,13 @@ import Link from 'next/link'
 
 const NotFound = () => {
   return (
-    <div className="flex flex-col w-dvh h-dvh justify-center items-center">
-      <h1>404 Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
-      <Link href="/" className='bg-gray-200 rounded h-10 w-50 justify-center items-center text-center'>Go back to home</Link>
-    </div>
+    <>
+      <div className='h-dvh w-dvw flex flex-col justify-center items-center space-y-2'>
+        <h1>404 Not Found</h1>
+        <p>The page you are looking for does not exist.</p>
+        <Link href="/" className='bg-gray-400 rounded-2xl px-4 py-2 hover:bg-gray-600 active:bg-gray-700 duration-150'>Go back to home</Link>
+      </div>
+    </>
   );
 };
 
