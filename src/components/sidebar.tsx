b9487bd0f38da4655d52ef7eff87befb098039e9
deefyouknow <deefyouknow@gmail.com>
2026-02-27 22:30:39 +0700

    deef

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -3,7 +3,7 @@ import { FaHome } from "react-icons/fa";
 export const Sidebar = () => {
   return (
     <>
-      <div className="bg-gray-100 h-full flex flex-col w-69 pt-10.25 pl-9 pr-5">
+      <div className="bg-[#f7fafc] h-full flex flex-col w-69 pt-10.25 pl-9 pr-5">
         {/*Dashboard*/}
         <div className="flex w-full gap-5">
           <VscGraphLine className="text-3xl" />
@@ -13,9 +13,11 @@ export const Sidebar = () => {
         {/*ส่วนของ Page*/}
         <div className="flex flex-col space-y-3 mt-3">
           {/*Dashboard*/}
-          <div className="bg-amber-300 rounded-2xl flex items-center  px-4 py-[12px] w-[220px] h-[54px]">
-            <FaHome />
-            <span>Dashboard</span>
+          <div className="bg-white rounded-2xl flex items-center  px-4 py-[12px] w-[220px] h-[54px]">
+            <div className="h-7.5 w-7.5 bg-[#4fd1c5] rounded-xl flex text-center justify-center items-center">
+              <FaHome className="text-white"/>
+            </div>
+            <span className="text-[16px] font-bold pl-2">Dashboard</span>
           </div>
         </div>
       </div>
